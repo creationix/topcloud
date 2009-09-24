04b4c3021f3b6b564c42ccab736cc4afa7117ec8
Timothy Caswell <tim@Timothy-Caswells-MacBook-Pro.local>
2009-09-24 16:53:35 -0500

    Add a remote data source example using set setTimeout as a simulated ajax

--- examples/Dialog.js
+++ examples/Dialog.js
@@ -1,9 +1,44 @@
 /*jslint white: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true */
 /*globals TC, jQuery, Dialog */
 
+// This simulates a remote resource proxy, normally this would be an object that ajaxes the
+// server to get and set data.  It even simulates a local cache to better test the dual nature
+// of the code.
+function TextSource() {
+	var cache, value;
+	
+	value = "This data is in a database";
+	
+	this.get = function (callback) {
+		if (cache === undefined) {
+			setTimeout(function () {
+				cache = value;
+				callback(cache);
+			}, 1000);
+			
+			return;
+		}
+		return cache;
+	};
+	
+	this.set = function (new_value) {
+		cache = new_value;
+		setTimeout(function () {
+			value = new_value;
+		}, 1000);
+	};
+	
+	this.invalidate = function () {
+		cache = undefined;
+	};
+	
+}
+
 (function ($) {
 	
 	Dialog = Object.create(TC.Controller);
+	
+	Dialog.bindData({proxy: new TextSource(), as: "remote_text"});
 	
 	Dialog.cancel = function (button) {
 		button.parent.parent.close();
